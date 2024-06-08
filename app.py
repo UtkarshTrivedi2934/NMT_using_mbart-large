1bc60e4d85345963d041db742471fe1c605e0979
Utkarsh Trivedi <104593332+UtkarshTrivedi2934@users.noreply.github.com>
2024-06-08 22:29:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-!pip install transformers
-!pip install streamlit
-!npm install localtunnel
+pip install transformers
+pip install streamlit
+npm install localtunnel
 
 from transformers import MBartForConditionalGeneration, MBart50TokenizerFast
 import streamlit as st
